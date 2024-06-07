2b6c007953f55fee3b023dac0413017deb87a2e5
Amanda Nascimento <116469203+amandarnascimento@users.noreply.github.com>
2024-06-07 10:53:42 -0300

    Update tabela_calendario.sql
    Inclusão da coluna quadrimestre, coluna segasex informando se o dia é entre segunda a sexta feira e a coluna feriado, que lê da tabela feriado se é feriado ou não e retorna em 0 ou 1

--- tabela_calendario.sql
+++ tabela_calendario.sql
@@ -7,16 +7,20 @@ CREATE TABLE D_CALENDARIO (
     [DIA] AS (DATEPART(DAY, [DATA])),
     [TRI_NUM] AS (DATEPART(QUARTER, [DATA])),
     [TRIMESTRE] AS CASE
-                        WHEN (DATEPART(QUARTER, [DATA])) = 1 THEN '1º Trimestre'
-                        WHEN (DATEPART(QUARTER, [DATA])) = 2 THEN '2º Trimestre'
-                        WHEN (DATEPART(QUARTER, [DATA])) = 3 THEN '3º Trimestre'
-                        WHEN (DATEPART(QUARTER, [DATA])) = 4 THEN '4º Trimestre' END,
+                        WHEN (DATEPART(QUARTER, [DATA])) = 1 THEN '1o Trimestre'
+                        WHEN (DATEPART(QUARTER, [DATA])) = 2 THEN '2o Trimestre'
+                        WHEN (DATEPART(QUARTER, [DATA])) = 3 THEN '3o Trimestre'
+                        WHEN (DATEPART(QUARTER, [DATA])) = 4 THEN '4o Trimestre' END,
+    [QUADRIMESTRE] AS CASE
+                          WHEN (DATEPART(MONTH, [DATA])) BETWEEN 1 AND 4 THEN '1o Quadrimestre'
+                          WHEN (DATEPART(MONTH, [DATA])) BETWEEN 5 AND 8 THEN '2o Quadrimestre'
+                          WHEN (DATEPART(MONTH, [DATA])) BETWEEN 9 AND 12 THEN '3o Quadrimestre' END,
     [QUINZENA] AS CASE
-                       WHEN DATEPART(DAY, [DATA]) > 15 THEN '2ª Quinzena'
-                       ELSE '1ª Quinzena' END,
+                       WHEN DATEPART(DAY, [DATA]) > 15 THEN '2a Quinzena'
+                       ELSE '1a Quinzena' END,
     [SEMESTRE] AS CASE
-                       WHEN DATEPART(QUARTER, [DATA]) <= 2 THEN '1º Semestre'
-                       ELSE '2º Semestre' END,
+                       WHEN DATEPART(QUARTER, [DATA]) <= 2 THEN '1o Semestre'
+                       ELSE '2o Semestre' END,
     [SEM_NUM] AS CASE
                       WHEN DATEPART(QUARTER, [DATA]) <= 2 THEN 1
                       ELSE 2 END,
@@ -26,7 +30,7 @@ CREATE TABLE D_CALENDARIO (
     [MES_EXTENSO] AS CASE
                           WHEN (DATEPART(MONTH, [DATA])) = 1 THEN 'Janeiro'
                           WHEN (DATEPART(MONTH, [DATA])) = 2 THEN 'Fevereiro'
-                          WHEN (DATEPART(MONTH, [DATA])) = 3 THEN 'Março'
+                          WHEN (DATEPART(MONTH, [DATA])) = 3 THEN 'MarÃ§o'
                           WHEN (DATEPART(MONTH, [DATA])) = 4 THEN 'Abril'
                           WHEN (DATEPART(MONTH, [DATA])) = 5 THEN 'Maio'
                           WHEN (DATEPART(MONTH, [DATA])) = 6 THEN 'Junho'
@@ -49,23 +53,38 @@ CREATE TABLE D_CALENDARIO (
                          WHEN (DATEPART(MONTH, [DATA])) = 10 THEN 'Out.' + CAST(DATEPART(YEAR, [DATA]) AS VARCHAR)
                          WHEN (DATEPART(MONTH, [DATA])) = 11 THEN 'Nov.' + CAST(DATEPART(YEAR, [DATA]) AS VARCHAR)
                          WHEN (DATEPART(MONTH, [DATA])) = 12 THEN 'Dez.' + CAST(DATEPART(YEAR, [DATA]) AS VARCHAR)END,
-    [MES_ANO] AS FORMAT([DATA], 'MM.yyyy'));
+    [MES_ANO] AS FORMAT([DATA], 'MM.yyyy'),
+    [SegASex] AS CASE
+                     WHEN DATENAME(WEEKDAY, [DATA]) IN ('sÃ¡bado', 'domingo') THEN 0
+                     ELSE 1
+                 END
+);
 
-DECLARE @DataInicio DATETIME,
-        @DataFim    DATETIME;
+DECLARE @DataInicio DATE,
+        @DataFim    DATE;
 
-SET @DataInicio = '31/12/2020';
-SET @DataFim = '31/12/2026';
+-- Definindo as datas no formato correto
+SET @DataInicio = '2020-12-31';
+SET @DataFim = '2026-12-31';
 
 TRUNCATE TABLE D_CALENDARIO;
 
 WHILE @DataInicio <= @DataFim
 BEGIN
-    SET @DataInicio = DATEADD(DAY, 1, @DataInicio);
     INSERT INTO D_CALENDARIO ([DATA])
     VALUES (@DataInicio);
+    SET @DataInicio = DATEADD(DAY, 1, @DataInicio);
 END
 
+-- Adicionando a coluna Feriado
+ALTER TABLE D_CALENDARIO ADD [Feriado] BIT;
+
+-- Atualizando a coluna Feriado com base na tabela feriados
+UPDATE D_CALENDARIO
+SET [Feriado] = CASE
+                    WHEN EXISTS (SELECT 1 FROM feriados WHERE feriados.FeriadoData = D_CALENDARIO.DATA) THEN 1
+                    ELSE 0
+                END;
 
 SELECT *
   FROM D_CALENDARIO;
